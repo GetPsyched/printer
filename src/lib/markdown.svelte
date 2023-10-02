7334dcdd9d77e5f7989e7003bd89d8a91cdd53bb
GetPsyched <priyanshutr@proton.me>
2023-10-02 11:40:15 +0530

    feat: style markdown tables

--- src/lib/markdown.svelte
+++ src/lib/markdown.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import * as marked from 'marked';
+  import { marked } from 'marked';
   export let source: string;
 </script>
 
@@ -12,5 +12,18 @@
     margin: 0;
     margin-top: 4px;
     padding-left: 15px;
+  }
+
+  div :global(table),
+  div :global(th),
+  div :global(td) {
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px black solid;
+  }
+
+  div :global(th),
+  div :global(td) {
+    padding: 0.25rem;
   }
 </style>
